b798419d766fe3d8f4f0b33ccba6b472b4ce8943
fredrik <fredrik@cshift.tech>
2024-08-15 12:10:42 +0200

    Add script for creating test data

---
+++ add_test_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Character
+curl -X POST http://localhost:8080/api/v1/lore-keeper/characters -d '{"name": "Linda", "description": "A baaaaad biish", "age": 32, "home": "earth"}'
+
+# Faction
+curl -X POST http://localhost:8080/api/v1/lore-keeper/factions -d '{"name": "Lindas crew", "description": "Lindas crew", "notableCharacters": ["David", "Mikaela"], "foundingDate": 0}'
+
+# City
+curl -X POST http://localhost:8080/api/v1/lore-keeper/cities -d '{"name": "Lindsville", "notableCharacters": ["David", "Mikaela"], "factions": ["name"], "description": "Lindas crew", "foundingDate": "väldigt länge sedan"}'
+
+# World
+curl -X POST http://localhost:8080/api/v1/lore-keeper/worlds -d '{"name": "Lindas värld", "description": "Lindas värld", "cities": ["Lindsville"]}'
